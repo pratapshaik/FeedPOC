28c15065fc43c742806679877317a77fdf183df3
shaikpratap <pratapshaik@gmail.com>
2019-02-28 19:09:04 +0530

    Initial Commit

---
+++ FeedPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FeedPOC
+//
+//  Created by pratap shaik on 28/02/19.
+//  Copyright © 2019 pratap shaik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
